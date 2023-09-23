32f75e0e3593241547a4efa841c309041ebc46ce
Brunosbpb <bruno.sbpb1997@gmail.com>
2023-09-23 16:26:18 -0300

    meu ultimo

--- src/index.js
+++ src/index.js
@@ -1,8 +1,45 @@
-//nome do herói
+let nome = "bruno"
 
-let heroi = "Senhor Z";
-let nivelDoHeroi = 10000
+let nivel = 7500
 
-if (nivelDoHeroi <= "imortal")
+if (nivel <= 1000) {
 
-console.log(nivelDoHeroi)
+  nivel = "ferro";
+
+} else if (nivel >= 1001 && nivel <= 2000) {
+
+  nivel = "bronze"
+
+} else if (nivel >= 2001 && nivel <= 5000) {
+
+  nivel = "prata"
+
+} else if (nivel >= 5001 && nivel <= 7000) {
+
+  nivel = "ouro"
+
+} else if (nivel >= 7001 && nivel <= 8000) {
+
+  nivel = "platina"
+
+} else if (nivel >= 8001 && nivel <= 9000) {
+
+  nivel = "ascedente"
+
+} else if (nivel >= 9001 && nivel <= 10000) {
+
+  nivel = "imortal"
+
+} else if (nivel >= 10001) {
+
+  nivel = "radiante"
+
+} else {nivel = imorrivel
+
+}
+
+console.log ("O heroi de nome " + nome + " está no nível " + nivel )
+
+  
+
+
